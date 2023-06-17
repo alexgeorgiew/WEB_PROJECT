dc531bdeaca61c1f2ad1b82662c6f250eddb2f7e
alexgeorgiew <77070782+alexgeorgiew@users.noreply.github.com>
2023-06-17 20:11:55 +0300

    export is working

--- php/index.php
+++ php/index.php
@@ -1,56 +1,62 @@
 <?php
-      
-        if(isset($_POST['wizard_btn'])) {
-            echo "This is WIZARD BTN that is selected";
+        function wizard() {
+            			echo "This is WIZARD BTN that is selected";
         }
-        if(isset($_POST['export_btn'])) {
-            echo "This is EXXPORT BTN that is selected";
-            
-            
-	$htmlContent = file_get_contents("http://localhost/Project/index.html");
-		
-	$DOM = new DOMDocument();
-	$DOM->loadHTML($htmlContent);
-	
-	$Header = $DOM->getElementsByTagName('th');
-	$Detail = $DOM->getElementsByTagName('td');
+        function export() {
+			    echo "This is Export BTN that is selected";
+			    
+			$htmlContent = file_get_contents("http://localhost/Project/index.html");
+				
+			$DOM = new DOMDocument();
+			$DOM->loadHTML($htmlContent);
+			
+			$Header = $DOM->getElementsByTagName('th');
+			$Detail = $DOM->getElementsByTagName('td');
 
-    //#Get header name of the table
-	foreach($Header as $NodeHeader) 
-	{
-		$aDataTableHeaderHTML[] = trim($NodeHeader->textContent);
-	}
-	//print_r($aDataTableHeaderHTML); die();
+		       //#Get header name of the table
+			foreach($Header as $NodeHeader) 
+			{
+				$aDataTableHeaderHTML[] = trim($NodeHeader->textContent);
+			}
+			//print_r($aDataTableHeaderHTML); die();
 
-	//#Get row data/detail table without header name as key
-	$i = 0;
-	$j = 0;
-	foreach($Detail as $sNodeDetail) 
-	{
-		$aDataTableDetailHTML[$j][] = trim($sNodeDetail->textContent);
-		$i = $i + 1;
-		$j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
-	}
-	//print_r($aDataTableDetailHTML); die();
-	
-	//#Get row data/detail table with header name as key and outer array index as row number
-	for($i = 0; $i < count($aDataTableDetailHTML); $i++)
-	{
-		for($j = 0; $j < count($aDataTableHeaderHTML); $j++)
-		{
-			$aTempData[$i][$aDataTableHeaderHTML[$j]] = $aDataTableDetailHTML[$i][$j];
-		}
-	}
-	$aDataTableDetailHTML = $aTempData; unset($aTempData);
-	print_r($aDataTableDetailHTML); die();
-	
-	
-	
-            /*$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-		$txt = "John Doe\n";
-		fwrite($myfile, $txt);
-		$txt = "Jane Doe\n";
-		fwrite($myfile, $txt);
-		fclose($myfile);*/
+			//#Get row data/detail table without header name as key
+			$i = 0;
+			$j = 0;
+			foreach($Detail as $sNodeDetail) 
+			{
+				$aDataTableDetailHTML[$j][] = trim($sNodeDetail->textContent);
+				$i = $i + 1;
+				$j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
+			}
+			//print_r($aDataTableDetailHTML); die();
+			
+			//#Get row data/detail table with header name as key and outer array index as row number
+			for($i = 0; $i < count($aDataTableDetailHTML); $i++)
+			{
+				for($j = 0; $j < count($aDataTableHeaderHTML); $j++)
+				{
+					$aTempData[$i][$aDataTableHeaderHTML[$j]] = $aDataTableDetailHTML[$i][$j];
+				}
+			}
+			$aDataTableDetailHTML = $aTempData; unset($aTempData);
+			//print_r($aDataTableDetailHTML);
+			
+			
+			
+			    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+			    $results = print_r($aDataTableDetailHTML, true);
+			    fwrite($myfile, $results);
+			    fclose($myfile);
+        }
+        
+        if($_POST['wizard']=="wizard_btn")
+        {
+        	wizard();
+        }
+        
+        if($_POST['export']=="export_btn")
+        {
+        	export();
         }
     ?>
