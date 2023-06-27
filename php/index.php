0f5cd1a8b6041998983fa495bfef19d266959996
alexgeorgiew <77070782+alexgeorgiew@users.noreply.github.com>
2023-06-27 21:26:59 +0300

    Change logic and properly showing data

--- php/index.php
+++ php/index.php
@@ -1,11 +1,6 @@
 <?php
-        function wizard() {
-            			echo "This is WIZARD BTN that is selected";
-            			echo "\n";
-        }
+      
         function export() {
-			    echo "This is Export BTN that is selected";
-			    echo "\n";
 			    
 			$htmlContent = file_get_contents("http://localhost/Project/index.html");
 				
@@ -60,19 +55,19 @@
 		$conn = new mysqli($servername, $username, $password);
 		// Check connection
 		if ($conn->connect_error) {
-		  die("Connection failed: " . $conn->connect_error);
+		  //die("Connection failed: " . $conn->connect_error);
+		  //return some value to know what happen
 		}
 
 		// Create database
 		$sql = "CREATE DATABASE myDB";
 		if ($conn->query($sql) === TRUE) {
-		  echo "Database created successfully";
-		  echo "\n";
+		  //return some value to know what happen
 		} else {
-		  echo "Error creating database: " . $conn->error;
-		  echo "\n";
+		 //return some value to know what happen
 		}
 		
+		//return some value to know what happen
 		
         }
         function create_database_table(){
@@ -85,7 +80,8 @@
 		$conn = new mysqli($servername, $username, $password, $dbname);
 		// Check connection
 		if ($conn->connect_error) {
-		  die("Connection failed: " . $conn->connect_error);
+		  //die("Connection failed: " . $conn->connect_error);
+		  //return some value to know what happen
 		}
 
 		// sql to create table
@@ -96,23 +92,45 @@
 		memory VARCHAR(50) NOT NULL,
 		vcpus VARCHAR(50) NOT NULL,
 		storage VARCHAR(50) NOT NULL,
-		io VARCHAR(50) NOT NULL,
-		node_on_demand_cost  DOUBLE PRECISION(5,2) NOT NULL,
-		node_reserved_cost DOUBLE PRECISION(5,2) NOT NULL,
+		day_cost  DOUBLE PRECISION(10,5) NOT NULL,
+		month_cost DOUBLE PRECISION(10,5) NOT NULL,
+		year_cost DOUBLE PRECISION(10,5) NOT NULL,
 		region VARCHAR(50) NOT NULL
 		)";
 
 		if ($conn->query($sql) === TRUE) {
-		  echo "Table Services created successfully";
-		  echo "\n";
+		  //echo "Table Services created successfully";
+		  //echo "\n";
+		  //return some value to know what happen
 		} else {
-		  echo "Error creating table: " . $conn->error;
-		  echo "\n";
+		  //echo "Error creating table: " . $conn->error;
+		  //echo "\n";
+		  //return some value to know what happen
 		}
-
+		//return some value to know what happen
 		$conn->close();
         }
-        function get_data_from_database($min_price_per_day, $max_price_per_day, $min_memory,$max_memory, $regions_names){
+        function get_csv_and_fill_database($conn)
+        {
+        
+            $file = fopen('AmazonEC2InstanceComparison.csv', 'r');
+            while (($data = fgetcsv($file)) !== false) {
+
+            				$values = implode("','", $data);
+            				$sql = "INSERT INTO Services (name, api_name,memory, vcpus, storage, day_cost, month_cost, year_cost, region) VALUES ('" . $values . "')";
+
+            				if ($conn->query($sql) === true) {
+//             					echo "Data inserted successfully";
+                                                //return some value to know what happen
+            				} 
+            				else {
+            					//echo "Error inserting data: " . $conn->error;
+            					//return some value to know what happen
+            				}
+            }  
+        }
+        
+        function get_data_from_database($min_price_per_day = "default", $max_price_per_day = "default", $min_memory =  "default",$max_memory =  "default", $regions_names = "default"){
         		$servername = "localhost";
 			$username = "root";
 			$password = "";
@@ -122,37 +140,31 @@
 			$conn = new mysqli($servername, $username, $password, $dbname);
 			// Check connection
 			if ($conn->connect_error) {
-			  die("Connection failed: " . $conn->connect_error);
+			  //die("Connection failed: " . $conn->connect_error);
+			  //return some value to know what happen
 			}
-
-			$file = fopen('AmazonEC2InstanceComparison.csv', 'r');
-
-            while (($data = fgetcsv($file)) !== false) {
-
-            				$values = implode("','", $data);
-            				$sql = "INSERT INTO Services (name, api_name,memory, vcpus, storage, io, node_on_demand_cost, node_reserved_cost, region) VALUES ('" . $values . "')";
-
-            				if ($conn->query($sql) === true) {
-//             					echo "Data inserted successfully";
-            				} else {
-            					echo "Error inserting data: " . $conn->error;
-            				}
-            				if ($conn->query($sql) === false) {
-                            //             					echo "Data inserted successfully";
-//                                         				} else {
-                              echo "Error inserting data: " . $conn->error;
-                            }
-            }
-
-			$query = "SELECT *, ((node_reserved_cost - node_on_demand_cost) / node_on_demand_cost) as percentage_change FROM Services WHERE memory > '{$min_memory}' ";
-// 			-- 			 node_on_demand_cost > '{$min_price_per_day}' AND node_on_demand_cost < '{$max_price_per_day}' AND memory > '{$min_memory}' AND memory < '{$max_memory}' AND region = '{$regions_names}' ";
+			
+			$query="SELECT * FROM Services";
+			$result = $conn->query($query);
+			if($result->num_rows == 0)//when database is empty get information from csv file
+			{
+				get_csv_and_fill_database($conn);
+			}
+			
+			
+			$query = "SELECT *, ((day_cost - month_cost) / month_cost) as percentage_change_month ,((day_cost - year_cost) / year_cost) as percentage_change_year FROM Services WHERE day_cost > '{$min_price_per_day}' AND day_cost < '{$max_price_per_day}' AND memory > '{$min_memory}' AND memory < '{$max_memory}' AND region = '{$regions_names}'";
+			
+			
+			if($regions_names == "default"){
+			 $query = "SELECT *, ((day_cost - month_cost) / month_cost) as percentage_change_month ,((day_cost - year_cost) / year_cost) as percentage_change_year FROM Services";
+			}
 			
 			$result = $conn->query($query);
 
 			if ($result->num_rows > 0) {
 			  // output data of each row
 			  while($row = $result->fetch_assoc()) {
-			  	$row_data= $row["name"] . "\n" . $row["api_name"] . "\n" . $row["memory"] . "\n" . $row["vcpus"] . "\n" . $row["storage"] . "\n" . $row["io"] . "\n" . $row["node_on_demand_cost"] . "\n" . $row["node_reserved_cost"] . "\n" . $row["region"] . "\n" . $row["percentage_change"];
+			  	$row_data= $row["name"] . "\n" . $row["api_name"] . "\n" . $row["memory"] . "\n" . $row["vcpus"] . "\n" . $row["storage"] . "\n" . $row["day_cost"] . "\n" . $row["month_cost"] . "\n" . $row["year_cost"] . "\n" . $row["region"] . "\n" . $row["percentage_change_month"] . "\n" . $row["percentage_change_year"] ;
 			    echo $row_data;
 			    echo "\n";
 			  }
@@ -164,41 +176,81 @@
 			
 			
 			$conn->close();
-        		
-        		
-        		
-        		/*$htmlContent = file_get_contents("http://localhost/Project/index.html");
-			$DOM = new DOMDocument();
-			$DOM->loadHTML($htmlContent);
-			
-			
-		        $para = $DOM->createElement("p");
-                        $node = $DOM->createTextNode("This is new.");
-                        $para->appendChild($node);
-                        $element = $DOM->getElementById("main");
-                        $element->appendChild($para);*/
-
         }
-//         }
+        
+        function get_information_for_news()
+        {
+        	        $servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "myDB";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+			  //die("Connection failed: " . $conn->connect_error);
+			  //return some value to know what happen
+			}
+			
+			$query="SELECT name,day_cost,month_cost,year_cost  FROM Services";
+			$result = $conn->query($query);
+
+			if ($result->num_rows > 0) {
+			  // output data of each row
+			  while($row = $result->fetch_assoc()) {
+			  	$row_data= $row["name"] . "\n" . $row["day_cost"] . "\n" . $row["month_cost"] . "\n" . $row["year_cost"] ;
+			    echo $row_data;
+			    echo "\n";
+			  }
+			}
+			else {
+			  echo "0 results";
+			  echo "\n";
+			}
+			
+			
+			$conn->close();
+        }
+        
+        create_database();
+        create_database_table();
         
         if($_POST['wizard']=="wizard_btn")
         {
-        	//wizard();
-//         	create_database();
-//         	create_database_table();
-        	get_data_from_database($_POST['min_price_per_day'],$_POST['max_price_per_day'],$_POST['min_memory'],$_POST['max_memory'],$_POST['regions_names']);
-        	/*echo $_POST['min_price_per_day'];
-        	echo "\n";
-        	echo $_POST['max_price_per_day'];
-        	echo "\n";
-        	echo $_POST['min_memory'];
-        	echo "\n";
-        	echo $_POST['max_memory'];
-        	echo "\n";
-        	echo $_POST['regions_names'];*/
+                $correct="1";
+        	if($_POST['min_price_per_day'] == ""){
+        		echo "Invalid minimum price per day\n";
+        		$correct="0";
+        	}
+        	if($_POST['max_price_per_day'] == ""){
+        		echo "Invalid maximum price per day\n";
+        		$correct="0";
+        	}
+        	if($_POST['min_price_per_day'] == ""){
+        		echo "Invalid minimum memory\n";
+        		$correct="0";
+        	}
+        	if($_POST['min_price_per_day'] == ""){
+        		echo "Invalid maximum mempry\n";
+        		$correct="0";
+        	}
+        	
+        	if($correct == "1")get_data_from_database($_POST['min_price_per_day'],$_POST['max_price_per_day'],$_POST['min_memory'],$_POST['max_memory'],$_POST['regions_names']);
         }
         else if($_POST['export']=="export_btn")
         {
         	export();
         }
+        else if($_POST['import']=="import_btn")
+        {
+         	get_data_from_database();
+        }
+        else if($_POST['news']=="news_btn")
+        {
+        	get_information_for_news();
+        }
+        
+        
+        
     ?>
